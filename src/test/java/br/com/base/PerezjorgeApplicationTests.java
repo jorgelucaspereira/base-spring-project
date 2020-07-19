17ed250152ad5500a6fd3e470f0284e5cc2a0855
Jorge <jorgelucassouzapereira@gmail.com>
2020-07-19 11:36:51 -0300

    Initial commit

---
+++ src/test/java/br/com/base/PerezjorgeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerezjorgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
